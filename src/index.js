e2966607cc5fc4a91efbb4f623b13def502bfca7
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-06-08 17:30:41 +0530

    added reverse proxy

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api', apiRoutes);
+app.use('/flightsService/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server is running on port ${ServerConfig.PORT}`);
